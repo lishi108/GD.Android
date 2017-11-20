7e0b53ec3949e65a74a8a022f3c60d64c8946a3e
李世强 <1067913086@qq.com>
2017-11-20 09:04:14 +0800

    调整开发环境

---
+++ app/src/main/java/com/guodong/business/http/BaseEntity.java
@@ -0,0 +1,38 @@
+package com.guodong.business.http;
+
+import java.io.Serializable;
+
+/**
+ * Description:
+ * Created by LSQ108 on 2017/10/27.
+ */
+
+public class BaseEntity <T> implements Serializable {
+    private int code;
+    private String msg;
+    private T data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return msg;
+    }
+
+    public void setMessage(String message) {
+        this.msg = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
